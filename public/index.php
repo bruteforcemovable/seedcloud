606779cff3a76ed87503844d76986cb87eb3392c
Stefan Schnitzler <stefanschnitzler@MacBook-Pro.local>
2020-06-21 19:43:00 +0200

    moved index to proper location

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require_once './../vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('./views');
 $twig = new Twig_Environment($loader, array(
-//	'cache' => './views_cache'
+//    'cache' => './views_cache'
 ));
 
 $router = new \SeedCloud\Router($twig);
